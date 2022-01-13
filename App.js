aa808764c9c88ba371e6b9cf45ed5a28d00b56ed
unknown <iamshujasaqib@gmail.com>
2022-01-13 23:04:17 +0500

    second commit

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import { ImageBackground, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity,ImageBackground, SafeAreaView, StyleSheet, Text, View, Alert } from 'react-native';
 
 const image = require("./sources/assets/Background.jpeg")
 
 export default function App() {
   return (
-    <ImageBackground source={image} style={{flex:1}}>
+    <ImageBackground source={image} style={styles.imageBackgroundContainer}>
+      <SafeAreaView>
+        <View style={{alignItems:'center', marginTop:130}}>
+          <Text style={[styles.textStyle, {fontSize:60} ]}>GETOFFER</Text>
+        </View>
+        <View style={{alignItems:'center', marginTop:20}}>
+          <Text style={[styles.textStyle, {fontSize:50} ]}>WELCOME</Text>
+        </View>
+        <View style={{alignItems:'center', marginTop:10}}>
+          <Text style={[styles.textStyle, {fontSize:20} ]}>NICE TO SEE YOU</Text>
+        </View>
+        
+        <TouchableOpacity style={[styles.touchableStyle,{marginTop:50}]} onPress={()=>Alert.alert("Login alert")}>
+          <Text style={{fontSize:20, color:'#D3D3D3'}}>Login</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.touchableStyle} onPress={()=>Alert.alert("Registeration alert")}>
+          <Text style={{fontSize:20, color:'#D3D3D3'}}>Registeration</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.touchableStyle} onPress={()=>Alert.alert("Guest alert")}>
+          <Text style={{fontSize:20, color:'#D3D3D3'}}>Use as Guest</Text>
+        </TouchableOpacity>
+        
+
+      </SafeAreaView>
 
     </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  imageBackgroundContainer:{
+    flex:1,
+    flexDirection:'column'
   },
+  textStyle:{
+    color:'white',
+    fontWeight:'bold',
+    color:'silver',
+    fontFamily:'Courier New'
+    },
+    touchableStyle:{
+      backgroundColor:'rgba(83, 110, 115, 0.6)',
+      alignItems:'center',
+      alignSelf:'center',
+      justifyContent:'center',
+      marginTop:20,
+      width:'65%',
+      height:'7.5%',
+      borderRadius:10,
+    }
 });
